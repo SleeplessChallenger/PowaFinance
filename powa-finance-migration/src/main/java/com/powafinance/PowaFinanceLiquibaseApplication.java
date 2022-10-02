c248eb180c9e9709232c7a32d640e417e1ec419b
Daniil Slobodeniuk <sausernsunny@gmail.com>
2022-10-02 12:01:43 +0300

    Fully configured liquibase

---
+++ powa-finance-migration/src/main/java/com/powafinance/PowaFinanceLiquibaseApplication.java
@@ -0,0 +1,11 @@
+package com.powafinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowaFinanceLiquibaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PowaFinanceLiquibaseApplication.class, args);
+    }
+}
